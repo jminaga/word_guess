5780cf0cd236a85a10b1b95ce506ba4d8b50b1e3
Jared Minaga <jdm1code@gmail.com>
2023-08-01 19:35:09 -0600

    Re-did method of rendering blanks to screen

--- assets/script.js
+++ assets/script.js
@@ -2,54 +2,31 @@ const startBox = document.querySelector("#start-box");
 const startButton = document.querySelector("#start-button");
 const timer = document.querySelector("#timer");
 const timerBox = document.querySelector("#timer-box");
-const wordBox = document.querySelector("#word-box")
+const wordBox = document.querySelector("#word-box");
 const words = document.querySelector("#words");
-const placeHolder = document.querySelector(".placeHolder");
+const placeHolder = document.querySelector("#placeHolder");
+const getLetter = document.querySelector("data-palceholder");
 
 const wordBank = ["dog", "cat", "fish", "cow", "bird", "bug", "abracadbra"];
 const result = [];
 
 let secondsLeft = 30;
-let wordChoice = ""
+let wordChoice = "";
 
 
-// Creates blank spaces and renders to section
-// const renderBlanks = () => {
-//   wordChoice = wordBank[Math.floor(Math.random() * wordBank.length)];
-//   choiceLetters = wordChoice.split("")
-//   numberOfBlanks = choiceLetters.length
-//   blankArray = []
-//   for (let i = 0; i < numberOfBlanks; i++){
-//     blankArray.push("_")
-//   }
-//   placeHolder.textContent = blankArray.join(" ")
-//   console.log(wordChoice)
-//   console.log(blankArray)
-// }  
-
-const renderBlanks = () => {
-  // Generates random word from word bank
+const renderBlanks = () =>{
   wordChoice = wordBank[Math.floor(Math.random() * wordBank.length)];
-  choiceLetters = wordChoice.split("");
+    choiceLetters = wordChoice.split("");
+    numberOfBlanks = wordChoice.length;
+    console.log(choiceLetters)
+    console.log(numberOfBlanks)
 
-  for (let i = 0; i < choiceLetters.length; i++) {
-    const letter = choiceLetters[i]
-    const letterPlaceHolder = document.createElement("div")
-    letterPlaceHolder.setAttribute("id", "placeHolder")
-    letterPlaceHolder.textContent = "_";
-    wordBox.append(letterPlaceHolder);
+ for (var i = 0; i < numberOfBlanks; i++) {
+  result.push("_")
 
-console.log(letter)
-
-
-  }
-  console.log(choiceLetters)
-
-  console.log(wordChoice)
-}
-
-const letterGuess = () => {
-
+ }
+wordBox.textContent = result.join(" ")
+console.log(result)
 }
 
 
@@ -73,11 +50,12 @@ const setTime = () => {
 const gameStart = () => {
   startButton.addEventListener("click", function () {
     startBox.setAttribute("style", "display:none");
-    wordBox.setAttribute("style", "display:flex")
+    wordBox.setAttribute("style", "display:flex");
     timerBox.setAttribute("style", "display:flex");
     setTime();
   });
 };
 
 gameStart();
-renderBlanks()
+renderBlanks();
+
